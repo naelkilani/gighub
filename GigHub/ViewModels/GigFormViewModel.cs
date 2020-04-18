41006720bbed4f4b83fc8ec94ad5fac543114afb
naelkailani <naelkilanii@gmail.com>
2020-04-19 00:22:38 +0300

    refactoring.

--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -7,6 +7,6 @@ namespace GigHub.ViewModels
     {
         public GigDto GigDto { get; set; } = new GigDto();
         public IEnumerable<GenreDto> GenreDtos { get; set; } = new List<GenreDto>();
-        public string Title => GigDto.Id != 0 ? "Update a Gig" : "Add a Gig";
+        public string Heading => GigDto.Id != 0 ? "Update a Gig" : "Add a Gig";
     }
 }
