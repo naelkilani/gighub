4daf4c725441d7c5cf4a0edad6868955fbdeaa99
naelkailani <naelkilanii@gmail.com>
2020-04-19 16:50:53 +0300

    implement logical delete (cancel) for gigs.

---
+++ GigHub/Controllers/Api/GigsController.cs
@@ -0,0 +1,33 @@
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers.Api
+{
+    [Authorize]
+    public class GigsController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Cancel(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var gig = _context.Gigs.FirstOrDefault(g => g.Id == id && g.ArtistId == userId);
+
+            if (gig == null || !gig.Active)
+                return NotFound();
+
+            gig.Active = false;
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
