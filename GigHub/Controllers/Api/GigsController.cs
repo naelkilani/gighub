759d1c557583609b6806bcd38cf6e510a0ccdea0
naelkailani <naelkilanii@gmail.com>
2020-04-25 17:04:02 +0300

    support canceled notifications.

--- GigHub/Controllers/Api/GigsController.cs
+++ GigHub/Controllers/Api/GigsController.cs
@@ -1,5 +1,7 @@
 ﻿using GigHub.Models;
 using Microsoft.AspNet.Identity;
+using System;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Http;
 
@@ -19,15 +21,41 @@ namespace GigHub.Controllers.Api
         public IHttpActionResult Cancel(int id)
         {
             var userId = User.Identity.GetUserId();
-            var gig = _context.Gigs.FirstOrDefault(g => g.Id == id && g.ArtistId == userId);
+            var gig = _context.Gigs.Include(g => g.Attendees).FirstOrDefault(g => g.Id == id && g.ArtistId == userId);
 
             if (gig == null || !gig.Active)
                 return NotFound();
 
             gig.Active = false;
+
+            AddCanceledNotifications(gig);
+
             _context.SaveChanges();
 
             return Ok();
+        }
+
+        private void AddCanceledNotifications(Gig gig)
+        {
+            var notification = new Notification
+            {
+                GigId = gig.Id,
+                DateTime = DateTime.Now,
+                Type = NotificationType.GigCanceled
+            };
+
+            _context.Notifications.Add(notification);
+
+            foreach (var attendee in gig.Attendees)
+            {
+                var userNotification = new UserNotification
+                {
+                    UserId = attendee.Id,
+                    NotificationId = notification.Id
+                };
+
+                _context.UserNotifications.Add(userNotification);
+            }
         }
     }
 }
