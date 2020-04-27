4834b7676dd3b5245ca1fc452658d1f2a39b30a6
naelkailani <naelkilanii@gmail.com>
2020-04-27 03:42:29 +0300

    current work on getting 5 notifications at minimum.

--- GigHub/Controllers/Api/NotificationsController.cs
+++ GigHub/Controllers/Api/NotificationsController.cs
@@ -2,6 +2,7 @@
 using GigHub.Dtos;
 using GigHub.Models;
 using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web.Http;
@@ -11,6 +12,8 @@ namespace GigHub.Controllers.Api
     [Authorize]
     public class NotificationsController : ApiController
     {
+        private const int MinimumNotificationToDisplay = 5;
+
         private readonly ApplicationDbContext _context;
 
         public NotificationsController()
@@ -23,15 +26,32 @@ namespace GigHub.Controllers.Api
         {
             var userId = User.Identity.GetUserId();
 
-            var notificationDtos = _context.Users
+            var userNotifications = _context.Users
                 .Include(u => u.UserNotifications.Select(un => un.Notification.Gig.Artist))
                 .First(u => u.Id == userId)
-                .UserNotifications
-                .Where(un => !un.IsRead)
+                .UserNotifications;
+
+            var unreadNotifications = GetUnreadNotifications(userNotifications);
+
+            if (unreadNotifications.Count - MinimumNotificationToDisplay > 0)
+                return Ok(unreadNotifications);
+
+            var notifications = userNotifications
                 .Select(un => un.Notification)
+                .OrderByDescending(u => u.DateTime)
+                .Take(MinimumNotificationToDisplay)
                 .Select(Mapper.Map<Notification, NotificationDto>);
 
-            return Ok(notificationDtos);
+            return Ok(notifications);
+        }
+
+        private List<NotificationDto> GetUnreadNotifications(ICollection<UserNotification> userNotifications)
+        {
+            return userNotifications
+                .Where(un => !un.IsRead)
+                .Select(un => un.Notification)
+                .Select(Mapper.Map<Notification, NotificationDto>)
+                .ToList();
         }
 
         [HttpPost]
