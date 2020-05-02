66cebabe96096ce58d81d3c597cdcfead7b974ea
naelkailani <naelkilanii@gmail.com>
2020-05-02 15:05:14 +0300

    Add the saeach field to the HOME page with using input-group.

--- GigHub/ViewModels/GigsViewModel.cs
+++ GigHub/ViewModels/GigsViewModel.cs
@@ -7,5 +7,6 @@ namespace GigHub.ViewModels
     {
         public IEnumerable<GigDto> GigDtos = new List<GigDto>();
         public string Heading { get; set; }
+        public string SearchTerm { get; set; }
     }
 }
